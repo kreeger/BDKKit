1bbe915d346043c6ceb27fa96ee32999a8d6393a
Ben Kreeger <ben@kree.gr>
2013-05-05 12:36:09 -0500

    Adding code from other projects.

--- Classes/Foundation/NSString+BDKKit.h
+++ Classes/Foundation/NSString+BDKKit.h
@@ -1,7 +1,27 @@
 #import <Foundation/Foundation.h>
 
+/** These are handy wrappers around various messages that interact with strings.
+ */
 @interface NSString (BDKKit)
 
+/** Checks for nil, NSNull, or blank.
+ *  @returns `YES` if string is falsy.
+ */
+- (BOOL)isEmptyOrNull;
+
+/** Shorthand for NSURL's `URLWithString:`.
+ *  @returns a URL with self.
+ */
+- (NSURL *)urlValue;
+
+/** URL-encodes a string.
+ *  @returns a URL-encoded version of self.
+ */
+- (NSString *)stringByUrlEncoding;
+
+/** Returns a string modified by capitalizing the first letter.
+ *  @returns A new string with a capitalized first letter.
+ */
 - (NSString *)stringByCapitalizingFirstLetter;
 
 @end
--- Classes/Foundation/NSString+BDKKit.m
+++ Classes/Foundation/NSString+BDKKit.m
@@ -2,6 +2,18 @@
 
 @implementation NSString (BDKKit)
 
+- (BOOL)isEmptyOrNull {
+    return self == nil || [(NSNull *)self isEqual:[NSNull null]] || [self isEqual:@""];
+}
+
+- (NSURL *)urlValue {
+    return [NSURL URLWithString:self];
+}
+
+- (NSString *)stringByUrlEncoding {
+    return (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL, (CFStringRef)self, NULL, (CFStringRef)@"!*'();:@&amp;=+$,/?%#[]", kCFStringEncodingUTF8));
+}
+
 - (NSString *)stringByCapitalizingFirstLetter {
     return [self stringByReplacingCharactersInRange:NSMakeRange(0,1)
                                          withString:[[self substringToIndex:1] uppercaseString]];
