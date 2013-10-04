6c30aece39dbf0e7d58f020956d00660be602b1f
Ben Kreeger <ben@kree.gr>
2013-10-04 13:23:44 -0500

    Adding block-based UIAlertView stuff.

--- Classes/UIKit/UIAlertView+BDKKit.h
+++ Classes/UIKit/UIAlertView+BDKKit.h
@@ -1,9 +1,24 @@
 #import <UIKit/UIKit.h>
 
-/** A convenience wrapper for some UIAlertView stuff.
+/**
+ A convenience wrapper for some UIAlertView stuff.
  */
 @interface UIAlertView (BDKKit)
 
+/**
+ If you don't need a delegate, this creates and shows a UIAlertView all in one.
+ 
+ @param title The alert view title.
+ @param message The body message of the alert.
+ @param closeText The message that will show up in the only button to dismiss the alert.
+ */
 + (void)showSimpleAlertWithTitle:(NSString *)title message:(NSString *)message closeText:(NSString *)closeText;
+
+/**
+ In place of a delegate, this calls show on an alert view and passes delegate methods to this block instead.
+ 
+ @param completion A block to be called when a user taps on a button.
+ */
+- (void)showWithCompletion:(void(^)(UIAlertView *alertView, NSInteger buttonIndex))completion;
 
 @end
--- Classes/UIKit/UIAlertView+BDKKit.m
+++ Classes/UIKit/UIAlertView+BDKKit.m
@@ -1,5 +1,29 @@
 #import "UIAlertView+BDKKit.h"
 
+#import <objc/runtime.h>
+
+@interface BDKAlertViewBlockHandler : NSObject
+
+@property (copy, nonatomic) void (^completionBlock)(UIAlertView *alertView, NSInteger buttonIndex);
+
+@end
+
+@implementation BDKAlertViewBlockHandler
+
+#pragma mark - UIAlertViewDelegate
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (self.completionBlock) self.completionBlock(alertView, buttonIndex);
+}
+
+- (void)alertViewCancel:(UIAlertView *)alertView {
+    if (self.completionBlock) self.completionBlock(alertView, alertView.cancelButtonIndex);
+}
+
+@end
+
+static const char BDKAlertViewBlockHandlerKey;
+
 @implementation UIAlertView (BDKKit)
 
 + (void)showSimpleAlertWithTitle:(NSString *)title message:(NSString *)message closeText:(NSString *)closeText {
@@ -8,6 +32,14 @@
                         delegate:nil
                cancelButtonTitle:closeText
                otherButtonTitles:nil] show];
+}
+
+- (void)showWithCompletion:(void (^)(UIAlertView *, NSInteger))completion {
+    BDKAlertViewBlockHandler *handler = [BDKAlertViewBlockHandler new];
+    handler.completionBlock = completion;
+    self.delegate = handler;
+    objc_setAssociatedObject(self, &BDKAlertViewBlockHandlerKey, handler, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    [self show];
 }
 
 @end
